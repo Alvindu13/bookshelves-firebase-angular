a170fb90ebabb3021e2e2aaa07ef362127e1c7e5
jeremie <alcaraz.jeremie@hotmail.fr>
2019-08-15 23:27:34 +0200

    release 1.0.0

--- src/app/book-list/single-book/single-book.component.ts
+++ src/app/book-list/single-book/single-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {BookModel} from '../../models/Book.model';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BooksService} from '../../services/books.service';
 
 @Component({
   selector: 'app-single-book',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleBookComponent implements OnInit {
 
-  constructor() { }
+  book: BookModel;
+
+  constructor(private route: ActivatedRoute,
+              private booksService: BooksService,
+              private router : Router) { }
 
   ngOnInit() {
+    //tempoaraire pour éviter les erreurs
+    this.book = new BookModel('', '');
+    const id = this.route.snapshot.params['id'];
+    this.booksService.getSinglebook(+id).then(
+      (book : BookModel) => {
+        this.book = book;
+      }
+    );
+  }
+
+  onBack() {
+    this.router.navigate(['/books']);
   }
 
 }
