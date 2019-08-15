575bed0ec967bd1d130d74b8d3fe38ee6f3f7823
jeremie <alcaraz.jeremie@hotmail.fr>
2019-08-15 22:02:30 +0200

    create method for signin, signout and create an new user form signup html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves-firebase';
+
+  constructor() {
+    // Your web app's Firebase configuration
+    let firebaseConfig = {
+      apiKey: "AIzaSyCULOKoUdhOCxysQm9Xx2Mvtc7nN1UVf8k",
+      authDomain: "bookshelves-dd9c1.firebaseapp.com",
+      databaseURL: "https://bookshelves-dd9c1.firebaseio.com",
+      projectId: "bookshelves-dd9c1",
+      storageBucket: "bookshelves-dd9c1.appspot.com",
+      messagingSenderId: "77511929551",
+      appId: "1:77511929551:web:9cb2c549891ae6c7"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
